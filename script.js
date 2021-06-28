7c75fcd923a2bf9c2d19b9c61ca926a1c6745192
sohamb17 <sohambelurgikar17@gmail.com>
2021-06-29 02:17:31 +0530

    Set up all basic functionalities of the calculator

--- script.js
+++ script.js
@@ -11,7 +11,11 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return a / b;
+    if(b === 0) {
+        display.setAttribute('style', 'text-align: center');
+        return 'Syntax Error';
+    }
+    return Math.round((a / b) * 1000000) / 1000000;
 }
 
 function operate(a, operator, b) {
@@ -25,3 +29,62 @@ function operate(a, operator, b) {
         return divide(a, b);
     }
 }
+
+const buttonsContainer = document.querySelector('#buttons-container');
+const buttons = buttonsContainer.querySelectorAll('button');
+const display = document.querySelector('#display');
+let firstOperand = '';
+let operator = '';
+let secondOperand = '';
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        if(display.value === 'Syntax Error') {
+            display.value = '';
+            display.setAttribute('style', 'text-align: end');
+        }
+        if(button.textContent !== 'C') {
+            if(button.textContent !== '=') {
+                display.value += button.textContent;
+            }
+            if(button.textContent >=0 && button.textContent <= 9) {
+                if(!operator) {
+                    firstOperand += button.textContent;
+                } else if(operator === '=') {
+                    display.value = button.textContent;
+                    firstOperand = button.textContent;
+                    operator = '';
+                } else {
+                    secondOperand += button.textContent;
+                }
+            } else {
+                if(button.textContent === '+' || button.textContent === '-' || button.textContent === '*' || button.textContent === '/') {
+                    if(!secondOperand) {
+                        operator = button.textContent;
+                    } else {
+                        display.value = operate(parseInt(firstOperand), operator, parseInt(secondOperand));
+                        display.value += button.textContent;
+                        firstOperand = operate(parseInt(firstOperand), operator, parseInt(secondOperand));
+                        operator = button.textContent;
+                        secondOperand = '';
+                    }
+                } else if(button.textContent === '=' && firstOperand && secondOperand) {
+                    display.value = operate(parseInt(firstOperand), operator, parseInt(secondOperand));
+                    firstOperand = operate(parseInt(firstOperand), operator, parseInt(secondOperand));
+                    operator = '=';
+                    secondOperand = '';
+                } else if(button.textContent === '=' && firstOperand && operator){
+                    display.value = 'Syntax Error';
+                    display.setAttribute('style', 'text-align: center;');
+                    firstOperand = '';
+                    operator = '';
+                    secondOperand = '';
+                }
+            }
+        } else {
+            display.value = '';
+            firstOperand = '';
+            operator = '';
+            secondOperand = '';
+        }
+    });
+});
