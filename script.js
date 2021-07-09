fb4bdf494bd30c1b1e407dde07e8e4060c96260d
sohamb17 <sohambelurgikar17@gmail.com>
2021-07-10 02:00:10 +0530

    Improve decimal functionality

--- script.js
+++ script.js
@@ -52,8 +52,10 @@ operators.forEach(operator => {
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-        operators.forEach(operator => operator.disabled = false);
-        operatorsDisabled = false;
+        if(button.textContent !== '=') {
+            operators.forEach(operator => operator.disabled = false);
+            operatorsDisabled = false;
+        }
         if(display.value === 'Syntax Error') {
             display.value = '';
             firstOperand = '';
@@ -88,20 +90,36 @@ buttons.forEach(button => {
                     operatorsDisabled = true;
                 });
             } else {
-                if(button.textContent !== '=') {
+                if(button.textContent !== '=' && button.textContent !== '.') {
                     display.value += button.textContent;
                 }
                 if((button.textContent >= 0 && button.textContent <= 9) || button.textContent === '.') {
                     if(button.textContent === '.') {
                         button.disabled = true;
+                        if(!operator || secondOperand) {
+                            display.value += '.';
+                        }
                     }
                     if(!operator) {
+                        if(!firstOperand && button.textContent === '.') {
+                            firstOperand = '0';
+                            display.value = '0.';
+                        }
                         firstOperand += button.textContent;
                     } else if(operator === '=') {
-                        display.value = button.textContent;
-                        firstOperand = button.textContent;
+                        if(button.textContent === '.') {
+                            firstOperand = '0.';
+                            display.value = '0.';
+                        } else {
+                            firstOperand = button.textContent;
+                            display.value = button.textContent;
+                        }
                         operator = '';
                     } else {
+                        if(!secondOperand && button.textContent === '.') {
+                            secondOperand = '0';
+                            display.value += '0.';
+                        }
                         secondOperand += button.textContent;
                         operators.forEach(operator => operator.disabled = false);
                         operatorsDisabled = false;
@@ -152,113 +170,129 @@ buttons.forEach(button => {
 });
 
 display.addEventListener('keydown', (e) => {
-   if(e.key === '=' || e.key === 'Enter' || e.key === 'C' || e.key === 'X' || e.key === 'Backspace' || (e.key === '.' && decimal.disabled) || ((e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/') && operatorsDisabled)) {
+   /* if(e.key === '=' || e.key === 'Enter' || e.key === 'C' || e.key === 'X' || e.key === 'Backspace' || (e.key === '.' && decimal.disabled) || ((e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/') && operatorsDisabled)) {
        e.preventDefault();
-   } 
+   } */
+   if(!((e.key >= '0' && e.key <= '9') || ((e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/') && !operatorsDisabled)/*  || (e.key === '.' && !decimal.disabled) */)) {
+       e.preventDefault();
+   }
 });
 
 display.addEventListener('keydown', (e) => {
-    if(firstOperand || !(e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/')) {
-        operators.forEach(operator => operator.disabled = false);
-        operatorsDisabled = false;
-        if(display.value === 'Syntax Error') {
-            display.value = '';
-            firstOperand = '';
-            display.setAttribute('style', 'text-align: end');
-        }
-        if(e.key !== 'C') {
-            if(e.key === 'X' || e.key === 'Backspace') {
-                display.value = display.value.substring(0, display.value.length - 1);
-                if(!operator) {
-                    if(firstOperand[firstOperand.length - 1] === '.') {
-                        decimal.disabled = false;
-                    }
-                    firstOperand = firstOperand.substring(0, firstOperand.length - 1);
-                } else if(operator === '=') {
-                    if(`${firstOperand}`[`${firstOperand}`.length - 1] === '.') {
-                        decimal.disabled = false;
-                    }
-                    firstOperand = `${firstOperand}`.substring(0, `${firstOperand}`.length - 1);
-                    operator = '';
-                } else if(!secondOperand) {
-                    operator = ''
-                } else {
-                    if(secondOperand[secondOperand.length - 1] === '.') {
-                        decimal.disabled = false;
-                    }
-                    secondOperand = secondOperand.substring(0, secondOperand.length - 1);
-                }
-                operators.forEach(operator => {
-                    if(!firstOperand) {
-                        operator.disabled = true;
-                    }
-                    operatorsDisabled = true;
-                });
-            } else {
-                if((e.key >= '0' && e.key <= '9') || e.key === '.') {
-                    if(e.key === '.' && !decimal.disabled) {
-                        decimal.disabled = true;
-                        if(!operator) {
-                            firstOperand += e.key;
-                        } else {
-                            secondOperand += e.key;
-                            operators.forEach(operator => operator.disabled = false);
-                            operatorsDisabled = false;
+    if((e.key >= '0' && e.key <= '9') || e.key === '=' || e.key === 'Enter' || e.key === 'C' || e.key === 'X' || e.key === 'Backspace' || e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/' || e.key === '.') {
+        if(firstOperand || !(e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/')) {
+            if(e.key !== '=' && e.key !== 'Enter') {
+                operators.forEach(operator => operator.disabled = false);
+                operatorsDisabled = false;
+            }
+            if(display.value === 'Syntax Error') {
+                display.value = '';
+                firstOperand = '';
+                display.setAttribute('style', 'text-align: end');
+            }
+            if(e.key !== 'C') {
+                if(e.key === 'X' || e.key === 'Backspace') {
+                    display.value = display.value.substring(0, display.value.length - 1);
+                    if(!operator) {
+                        if(firstOperand[firstOperand.length - 1] === '.') {
+                            decimal.disabled = false;
                         }
-                    }
-                    if(!decimal.disabled || e.key !== '.') {
-                        if(!operator) {
-                            firstOperand += e.key;
-                        } else if(operator === '=') {
-                            display.value = '';
-                            firstOperand = e.key;
-                            operator = '';
-                        } else {
-                            secondOperand += e.key;
-                            operators.forEach(operator => operator.disabled = false);
-                            operatorsDisabled = false;
+                        firstOperand = firstOperand.substring(0, firstOperand.length - 1);
+                    } else if(operator === '=') {
+                        if(`${firstOperand}`[`${firstOperand}`.length - 1] === '.') {
+                            decimal.disabled = false;
                         }
+                        firstOperand = `${firstOperand}`.substring(0, `${firstOperand}`.length - 1);
+                        operator = '';
+                    } else if(!secondOperand) {
+                        operator = ''
+                    } else {
+                        if(secondOperand[secondOperand.length - 1] === '.') {
+                            decimal.disabled = false;
+                        }
+                        secondOperand = secondOperand.substring(0, secondOperand.length - 1);
                     }
+                    operators.forEach(operator => {
+                        if(!firstOperand) {
+                            operator.disabled = true;
+                        }
+                        operatorsDisabled = true;
+                    });
                 } else {
-                    decimal.disabled = false;
-                    if((e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/') && !operatorsDisabled) {
-                        if(!secondOperand) {
-                            operator = e.key;
-                            operators.forEach(operator => operator.disabled = true);
-                            operatorsDisabled = true;
-                        } else {
+                    if((e.key >= '0' && e.key <= '9') || e.key === '.') {
+                        if(e.key === '.' && !decimal.disabled) {
+                            decimal.disabled = true;
+                            if(!operator) {
+                                if(!firstOperand) {
+                                    firstOperand = '0';
+                                    display.value = '0';
+                                }
+                                firstOperand += e.key;
+                                display.value += e.key;
+                            } else {
+                                if(!secondOperand) {
+                                    secondOperand = '0';
+                                    display.value += '0';
+                                }
+                                secondOperand += e.key;
+                                display.value += e.key;
+                                operators.forEach(operator => operator.disabled = false);
+                                operatorsDisabled = false;
+                            }
+                        }
+                        if(!decimal.disabled || e.key !== '.') {
+                            if(!operator) {
+                                firstOperand += e.key;
+                            } else if(operator === '=') {
+                                display.value = '';
+                                firstOperand = e.key;
+                                operator = '';
+                            } else {
+                                secondOperand += e.key;
+                                operators.forEach(operator => operator.disabled = false);
+                                operatorsDisabled = false;
+                            }
+                        }
+                    } else {
+                        decimal.disabled = false;
+                        if((e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/') && !operatorsDisabled) {
+                            if(!secondOperand) {
+                                operator = e.key;
+                                operators.forEach(operator => operator.disabled = true);
+                                operatorsDisabled = true;
+                            } else {
+                                display.value = operate(parseFloat(firstOperand), operator, parseFloat(secondOperand));
+                                firstOperand = operate(parseFloat(firstOperand), operator, parseFloat(secondOperand));
+                                operator = e.key;
+                                operators.forEach(operator => operator.disabled = true);
+                                operatorsDisabled = true;
+                                secondOperand = '';
+                            }
+                        } else if((e.key === '=' || e.key === 'Enter') && firstOperand && secondOperand) {
                             display.value = operate(parseFloat(firstOperand), operator, parseFloat(secondOperand));
-                            // display.value += e.key;
                             firstOperand = operate(parseFloat(firstOperand), operator, parseFloat(secondOperand));
-                            operator = e.key;
+                            operator = '=';
+                            secondOperand = '';
+                        } else if((e.key === '=' || e.key === 'Enter') && firstOperand && operator){
+                            display.value = 'Syntax Error';
+                            display.setAttribute('style', 'text-align: center;');
+                            firstOperand = '';
+                            operator = '';
                             operators.forEach(operator => operator.disabled = true);
                             operatorsDisabled = true;
                             secondOperand = '';
                         }
-                    } else if((e.key === '=' || e.key === 'Enter') && firstOperand && secondOperand) {
-                        display.value = operate(parseFloat(firstOperand), operator, parseFloat(secondOperand));
-                        firstOperand = operate(parseFloat(firstOperand), operator, parseFloat(secondOperand));
-                        operator = '=';
-                        secondOperand = '';
-                    } else if((e.key === '=' || e.key === 'Enter') && firstOperand && operator){
-                        display.value = 'Syntax Error';
-                        display.setAttribute('style', 'text-align: center;');
-                        firstOperand = '';
-                        operator = '';
-                        operators.forEach(operator => operator.disabled = true);
-                        operatorsDisabled = true;
-                        secondOperand = '';
                     }
                 }
+            } else {
+                display.value = '';
+                firstOperand = '';
+                operator = '';
+                secondOperand = '';
+                decimal.disabled = false;
+                operators.forEach(operator => operator.disabled = true);
+                operatorsDisabled = true;
             }
-        } else {
-            display.value = '';
-            firstOperand = '';
-            operator = '';
-            secondOperand = '';
-            decimal.disabled = false;
-            operators.forEach(operator => operator.disabled = true);
-            operatorsDisabled = true;
         }
     }
 });
